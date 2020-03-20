2fb0e781e692637e5a454796f529e1f81ea051b9
Joan Andrés <andres6936@live.com>
2020-03-20 08:32:05 -0500

    Implemented extract comments, deleted comments and deleted lines empty

--- src/com/jacon/Convert.java
+++ src/com/jacon/Convert.java
@@ -1,13 +1,20 @@
 package com.jacon;
 
+import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
+import java.lang.StringBuilder;
 
 final class Convert
 {
+    // Fields
+
     private List<String> buffer;
+
+    private String commentHead;
+
+    // Methods
 
     void convertXML( File file )
     {
@@ -20,6 +27,51 @@ final class Convert
             e.printStackTrace( );
         }
 
-        System.out.print( buffer.toString() );
+        extractCommentHead();
+        deleteComments();
+        deleteLinesEmpty();
+    }
+
+    private void extractCommentHead()
+    {
+        StringBuilder stringBuilder = new StringBuilder(  );
+
+        for ( String comment : buffer )
+        {
+            if (comment.startsWith( "#" ))
+            {
+                stringBuilder.append( comment );
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        commentHead = stringBuilder.toString();
+    }
+
+    private void deleteComments()
+    {
+        for ( int i = 0; i < buffer.size(); i++ )
+        {
+            if ( buffer.get( i ).startsWith( "#" ))
+            {
+                buffer.remove( i );
+                i -= 1;
+            }
+        }
+    }
+
+    private void deleteLinesEmpty()
+    {
+        for ( int i = 0; i < buffer.size(); i++ )
+        {
+            if (buffer.get( i ).isEmpty())
+            {
+                buffer.remove( i );
+                i -= 1;
+            }
+        }
     }
 }
