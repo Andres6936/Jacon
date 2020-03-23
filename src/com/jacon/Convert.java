00526af5480734528c7e814aa508ec7ea1c95c63
Joan Andrés <andres6936@live.com>
2020-03-22 19:00:05 -0500

    Reformat Code

--- src/com/jacon/Convert.java
+++ src/com/jacon/Convert.java
@@ -2,29 +2,28 @@ package com.jacon;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
-import java.util.Arrays;
-import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.lang.StringBuilder;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
 
 final class Convert extends WriterXML
 {
     // Fields
 
-    private List<String> buffer;
+    private List< String > buffer;
 
-    private Vector<Dictionary> dictionaryList;
+    private Vector< Dictionary > dictionaryList;
 
     private String commentHead;
 
     // Construct
 
-    Convert()
+    Convert( )
     {
-        dictionaryList = new Vector<>(  );
+        dictionaryList = new Vector<>( );
     }
 
     // Methods
@@ -33,20 +32,20 @@ final class Convert extends WriterXML
     {
         try
         {
-            buffer = Files.readAllLines( file.toPath() );
+            buffer = Files.readAllLines( file.toPath( ) );
         }
         catch ( IOException e )
         {
             e.printStackTrace( );
         }
 
-        extractCommentHead();
-        deleteComments();
-        deleteLinesEmpty();
-        mergeLinesSeparates();
-        fillDictionaryList();
-        deleteCharacterUnused();
-        createTags();
+        extractCommentHead( );
+        deleteComments( );
+        deleteLinesEmpty( );
+        mergeLinesSeparates( );
+        fillDictionaryList( );
+        deleteCharacterUnused( );
+        createTags( );
 
         try
         {
@@ -58,13 +57,13 @@ final class Convert extends WriterXML
         }
     }
 
-    private void extractCommentHead()
+    private void extractCommentHead( )
     {
-        StringBuilder stringBuilder = new StringBuilder(  );
+        StringBuilder stringBuilder = new StringBuilder( );
 
         for ( String comment : buffer )
         {
-            if (comment.startsWith( "#" ))
+            if ( comment.startsWith( "#" ) )
             {
                 stringBuilder.append( comment );
             }
@@ -74,95 +73,95 @@ final class Convert extends WriterXML
             }
         }
 
-        commentHead = stringBuilder.toString();
+        commentHead = stringBuilder.toString( );
     }
 
-    private void deleteComments()
-    {
-        for ( int i = 0; i < buffer.size(); i++ )
-        {
-            if ( buffer.get( i ).startsWith( "#" ))
-            {
-                buffer.remove( i );
-                i -= 1;
-            }
-        }
-    }
-
-    private void deleteLinesEmpty()
-    {
-        for ( int i = 0; i < buffer.size(); i++ )
-        {
-            if (buffer.get( i ).isEmpty())
-            {
-                buffer.remove( i );
-                i -= 1;
-            }
-        }
-    }
-
-    private void mergeLinesSeparates()
+    private void deleteComments( )
     {
         for ( int i = 0; i < buffer.size( ); i++ )
         {
-            if (buffer.get( i ).startsWith( "msgid " ))
+            if ( buffer.get( i ).startsWith( "#" ) )
             {
-                StringBuilder wordKey = new StringBuilder( 1024 );
-
-                wordKey.append( buffer.get( i ) );
-
-                while ( i + 1 < buffer.size() )
-                {
-                    if (! buffer.get( i + 1 ).startsWith( "msgstr " ))
-                    {
-                        wordKey.append( buffer.remove( i + 1 ) );
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
                 buffer.remove( i );
-                buffer.add( i, wordKey.toString() );
-            }
-            else if (buffer.get( i ).startsWith( "msgstr " ))
-            {
-                StringBuilder wordKey = new StringBuilder( 1024 );
-
-                wordKey.append( buffer.get( i ) );
-
-                while ( i + 1 < buffer.size() )
-                {
-                    if (! buffer.get( i + 1 ).startsWith( "msgid " ))
-                    {
-                        wordKey.append( buffer.remove( i + 1 ) );
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                buffer.remove( i );
-                buffer.add( i, wordKey.toString() );
+                i -= 1;
             }
         }
     }
 
-    private void fillDictionaryList()
+    private void deleteLinesEmpty( )
+    {
+        for ( int i = 0; i < buffer.size( ); i++ )
+        {
+            if ( buffer.get( i ).isEmpty( ) )
+            {
+                buffer.remove( i );
+                i -= 1;
+            }
+        }
+    }
+
+    private void mergeLinesSeparates( )
+    {
+        for ( int i = 0; i < buffer.size( ); i++ )
+        {
+            if ( buffer.get( i ).startsWith( "msgid " ) )
+            {
+                StringBuilder wordKey = new StringBuilder( 1024 );
+
+                wordKey.append( buffer.get( i ) );
+
+                while ( i + 1 < buffer.size( ) )
+                {
+                    if ( ! buffer.get( i + 1 ).startsWith( "msgstr " ) )
+                    {
+                        wordKey.append( buffer.remove( i + 1 ) );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                buffer.remove( i );
+                buffer.add( i, wordKey.toString( ) );
+            }
+            else if ( buffer.get( i ).startsWith( "msgstr " ) )
+            {
+                StringBuilder wordKey = new StringBuilder( 1024 );
+
+                wordKey.append( buffer.get( i ) );
+
+                while ( i + 1 < buffer.size( ) )
+                {
+                    if ( ! buffer.get( i + 1 ).startsWith( "msgid " ) )
+                    {
+                        wordKey.append( buffer.remove( i + 1 ) );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                buffer.remove( i );
+                buffer.add( i, wordKey.toString( ) );
+            }
+        }
+    }
+
+    private void fillDictionaryList( )
     {
         String key = "";
         String value = "";
 
-        for (String string : buffer)
+        for ( String string : buffer )
         {
-            if (string.startsWith( "msgid " ))
+            if ( string.startsWith( "msgid " ) )
             {
                 key = string;
                 continue;
             }
-            else if (string.startsWith( "msgstr " ))
+            else if ( string.startsWith( "msgstr " ) )
             {
                 value = string;
             }
@@ -175,9 +174,9 @@ final class Convert extends WriterXML
         }
     }
 
-    private void deleteCharacterUnused()
+    private void deleteCharacterUnused( )
     {
-        for (Dictionary word : dictionaryList)
+        for ( Dictionary word : dictionaryList )
         {
             word.deleteStringInKey( "msgid " );
             word.deleteStringInValue( "msgstr " );
@@ -198,23 +197,23 @@ final class Convert extends WriterXML
         }
     }
 
-    private void createTags()
+    private void createTags( )
     {
-        for (Dictionary word : dictionaryList)
+        for ( Dictionary word : dictionaryList )
         {
-            if (haveXWords( 5, word.key ))
+            if ( haveXWords( 5, word.key ) )
             {
                 word.setKey( generateKeyOfXWords( 5, word.key ) );
             }
-            else if (haveXWords( 4, word.key ))
+            else if ( haveXWords( 4, word.key ) )
             {
                 word.setKey( generateKeyOfXWords( 4, word.key ) );
             }
-            else if (haveXWords( 3, word.key ))
+            else if ( haveXWords( 3, word.key ) )
             {
                 word.setKey( generateKeyOfXWords( 3, word.key ) );
             }
-            else if (haveXWords( 2, word.key ))
+            else if ( haveXWords( 2, word.key ) )
             {
                 word.setKey( generateKeyOfXWords( 2, word.key ) );
             }
@@ -225,13 +224,13 @@ final class Convert extends WriterXML
         }
     }
 
-    private boolean haveXWords(int x, String string)
+    private boolean haveXWords( int x, String string )
     {
         int counterSpaceBlank = 0;
 
-        for ( int i = 0; i < string.length(); i++ )
+        for ( int i = 0; i < string.length( ); i++ )
         {
-            if (string.charAt( i ) == ' ')
+            if ( string.charAt( i ) == ' ' )
             {
                 counterSpaceBlank += 1;
             }
@@ -240,21 +239,21 @@ final class Convert extends WriterXML
         return counterSpaceBlank >= x - 1;
     }
 
-    private String generateKeyOfXWords(int x, String key)
+    private String generateKeyOfXWords( int x, String key )
     {
-        Vector<String> words = new Vector( Arrays.asList( key.split( "\\s" ) ) );
+        Vector< String > words = new Vector( Arrays.asList( key.split( "\\s" ) ) );
 
-        while (words.size() > x)
+        while ( words.size( ) > x )
         {
             int positionWordMoreShorter = 0;
-            int sizeWordMoreShorter = words.get( 0 ).length();
+            int sizeWordMoreShorter = words.get( 0 ).length( );
 
             for ( int i = 1; i < words.size( ); i++ )
             {
-                if (words.get( i ).length() < sizeWordMoreShorter)
+                if ( words.get( i ).length( ) < sizeWordMoreShorter )
                 {
                     positionWordMoreShorter = i;
-                    sizeWordMoreShorter = words.get( i ).length();
+                    sizeWordMoreShorter = words.get( i ).length( );
                 }
             }
 
@@ -263,24 +262,24 @@ final class Convert extends WriterXML
 
         StringBuilder newKey = new StringBuilder( 69 );
 
-        for(String word: words)
+        for ( String word : words )
         {
-            newKey.append( capitalizeWord( word ) ) ;
+            newKey.append( capitalizeWord( word ) );
         }
 
-        return newKey.toString();
+        return newKey.toString( );
     }
 
-    private String capitalizeWord(String string)
+    private String capitalizeWord( String string )
     {
-        char[] buffer = string.toCharArray();
+        char[] buffer = string.toCharArray( );
 
         for ( int i = 0; i < buffer.length; i++ )
         {
-            buffer[i] = Character.toLowerCase( buffer[i] );
+            buffer[ i ] = Character.toLowerCase( buffer[ i ] );
         }
 
-        buffer[0] = Character.toUpperCase( buffer[0] );
+        buffer[ 0 ] = Character.toUpperCase( buffer[ 0 ] );
 
         return new String( buffer );
     }
